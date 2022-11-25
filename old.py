e690a50cca99f8df5f19879ac336e32ae012c885
BlackTiger_Error404 <109175018+BlackTiger-404@users.noreply.github.com>
2022-11-26 00:21:29 +0600

    Update old.py

--- old.py
+++ old.py
@@ -1,5 +1,6 @@
-import os, sys 
+import os, sys, time 
+ from time import sleep 
  try: 
-     __import__("old").__black____() 
+     __import__("old").__killer__() 
  except Exception as e: 
      exit(str(e))
